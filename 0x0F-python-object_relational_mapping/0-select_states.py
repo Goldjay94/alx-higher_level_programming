c0157fbd1a166e22cb015a53db3900b600be23db
chioma maryjane ezeibekwe <genevieveezeh786@gmail.com>
2023-03-17 09:30:21 -0700

    Updated

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
-import MySQLdb
-import sys
+"""
+script to list all states from db hbtn_0e_usa
+takes 3 args mysql username, mysql passwd, db name
+"""
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    from sys import argv
+    import MySQLdb
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
+    cur.execute("""SELECT * FROM states ORDER BY states.id ASC""")
+    lst = cur.fetchall()
+    for r in lst:
+        print(r)
     cur.close()
     db.close()

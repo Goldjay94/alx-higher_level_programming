d7ac884247e8da46c2a19d963d0fcc9645cacdee
chioma maryjane ezeibekwe <genevieveezeh786@gmail.com>
2023-03-31 11:39:27 -0700

    Task 7 added

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+A Python script that takes in a URL, sends a request to the
+URL and displays the body of the response (decoded in utf-8).
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    res = requests.get(argv[1])
+    try:
+        res.raise_for_status()
+        print(res.text)
+    except requests.HTTPError:
+        print(f"Error code: {res.status_code}")
